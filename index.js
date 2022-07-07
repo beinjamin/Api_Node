d34760379c0aa2dcaefa90f8e07c121e88955068
Jaures <jauresbeinajmin@gmail.com>
2022-07-07 21:44:55 +0100

    implementation de la rout dans l'index

--- index.js
+++ index.js
@@ -2,4 +2,6 @@ const express = require('express');
 const app = express();
 require('./models/dbconfig');
 
+const postsRoutes = require('./routes/posts');
+
 app.listen(5500, () => console.log ('Serve started: 5500'));
