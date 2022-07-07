6784484c9dc484352b7980bd6d63dd32590af0c3
Jaures <jauresbeinajmin@gmail.com>
2022-07-07 21:42:51 +0100

    gestion routes

--- routes/postsController.js
+++ routes/postsController.js
@@ -2,3 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const { PostsModel } = require  ('.../nodels/postsModel');
+
+router.get('/', (req,res)=> {
+    PostsModel.find((err,docs )=> {
+        console.log(docs);
+    })
+})
+
+module.exports = router
