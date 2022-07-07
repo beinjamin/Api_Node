66f891da3689103c8a4801a2115e8b0dbb1f152d
Jaures <jauresbeinajmin@gmail.com>
2022-07-07 21:29:09 +0100

    creation des different attriburt de la table post

--- models/postsModel.js
+++ models/postsModel.js
@@ -1,1 +1,19 @@
 const mongoose = require("mongoose");
+
+const PostsModel = mongose.model (
+    "api-node",
+    {
+        author:{
+            type: String,
+            required: true 
+        },
+
+        message: {
+            type: String,
+            required: true
+        }
+
+    },
+
+    "posts"
+)
