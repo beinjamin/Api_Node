cf9ad617e8a0b6e0f7398322bf860d961ba966ef
Jaures <jauresbeinajmin@gmail.com>
2022-07-17 20:13:35 +0100

    Mise en place de la route update

--- routes/postsController.js
+++ routes/postsController.js
@@ -29,7 +29,27 @@ router.post('/', (req,res)=>{
 
 });
 //update
-route.put ('/:id', (req , res) => {
+router.put ("/:id", (req , res) => {
+    if (ObjectId.isValid (req.params.id))
+    return res.status(400).send("ID inconnu : " + req.params.id)
+    const updateRecord={
+        author: req.body.author,
+        message: req.body.message,
+    };
+    PostsModel.findByIdAndUpdate(
+        req.params.id,
+        {
+            $set: updateRecord
+        },
+        {
+            new: true
+        },
+        (err, docs)=>{
+            if(!err)
+            res.send(docs)
+            else console.log("Update error : " +err);
+        }
+    )
 
 })
 
